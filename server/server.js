323372d5e4c63286608910bb821bd5e8c2315926
djkean <djkean7@gmail.com>
2024-11-21 11:48:18 -0330

    Request for tables
    Created request for getting data from both tables

--- server/server.js
+++ server/server.js
@@ -6,10 +6,19 @@ const chalk = require("chalk");
 const path = require("path");
 const cors = require("cors");
 const connectToSchema = require("./connect");
+const router = express.Router();
 const port = process.env.PL_PORT;
 
 app.use(cors({ origin: process.env.PL_FRONT }));
 app.use(express.json());
+
+router.get("/pets", (req, res) => {
+  connectToSchema.query(`SELECT * FROM pets LEFT JOIN mypets ON pets.id = mypets.id`, [], (err, results) => {
+    if (err) throw err;
+    res.json({status: 200, error: null, response: results});
+  })
+})
+
 app.listen(port);
 
 console.log(chalk.cyan(`pet-log running on ${port}`));
